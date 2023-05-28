1f9193dc1e35ddb92ed161de0d1173c04cb6571e
Francisco Carabaza <pacocarabaza@gmail.com>
2023-05-28 11:25:51 +0200

    Update 2_accesorios.ino

--- NanoHex_C3_Pico_ESPNOW_V02/2_accesorios.ino
+++ NanoHex_C3_Pico_ESPNOW_V02/2_accesorios.ino
@@ -7,21 +7,3 @@ void iniciarAccesorios() {
   Serial.begin(115200);
 }
 
-/*
-void leerVoltajeBateria() {
-  adcBateria = analogRead(pinBateria);
-  mvBateria = map(adcBateria, 0, 3030, 0, 4200);  // 4.20 v son 3030 en el ADC
-  mvBateria = constrain(mvBateria, 0, 4200);
-  porcentajeBateria = map(mvBateria, 3000, 4200, 0, 100);
-  porcentajeBateria = constrain(porcentajeBateria, 0, 100);
-}
-*/
-
-void timerAccesorios() {
-  const int intervaloDeActualizacion = 1000;  // milisegundos entre cada ciclo de temporización
-  static unsigned long ultimaActualizacion;
-  if ((millis() - ultimaActualizacion) > intervaloDeActualizacion) {
-    ultimaActualizacion = millis();
-    //leerVoltajeBateria();
-  }
-}
